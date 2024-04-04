407ff741530ab24c7bd13dd5e3d98e042ba44d1d
EC2 Default User <ec2-user@ip-172-31-7-205.ap-northeast-2.compute.internal>
2024-04-04 08:14:37 +0000

    modify code

--- update-code.sh
+++ update-code.sh
@@ -2,20 +2,22 @@
 
 pattern="^[a-zA-Z]+(\.[a-zA-Z]+)+$"
 
-while true; do
-    # Prompt the user to enter a domain address
-    read -p "Enter a domain address: " domain
+if [[ $1 =~ $pattern ]]; then
+    echo "Domain address is valid."
+    echo "Updating a domain name in web server"
+else
+    echo "Invalid domain address. It should contain only alphabets separated by a single dot."
+    exit 1
+fi
 
-    if [[ $domain =~ $pattern ]]; then
-        echo "Domain address is valid."
-        break
-    else
-        echo "Invalid domain address. It should contain only alphabets separated by a single dot."
-    fi
-done
 
 word_to_replace="{domain}"
 
-sed -i "s/$word_to_replace/$domain/g" networklab/web-8080/index.php
-sed -i "s/$word_to_replace/$domain/g" networklab/web-8080/contact.php
-sed -i "s/$word_to_replace/$domain/g" networklab/web-8080/display_guestbook.php
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/index.php
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/contact.php
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/display_guestbook.php
+
+echo "Finish update the code"
+echo "################"
+echo "## GOOD LUCK ###"
+echo "################"
