c93a8c35209b826e1a53a6746d1ac6a2c02bd779
EC2 Default User <ec2-user@ip-172-31-7-205.ap-northeast-2.compute.internal>
2024-04-04 08:28:22 +0000

    update file

---
+++ 1.web-update-code.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pattern="^[a-zA-Z]+(\.[a-zA-Z]+)+$"
+
+if [[ $1 =~ $pattern ]]; then
+    echo "Domain address is valid."
+    echo "Updating a domain name in web server"
+else
+    echo "Invalid domain address. It should contain only alphabets separated by a single dot."
+    exit 1
+fi
+
+
+word_to_replace="{domain}"
+
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/index.php
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/contact.php
+sed -i "s/$word_to_replace/$1/g" networklab/web-8080/display_guestbook.php
+
+echo "Finish update the code"
+
+echo "Copy the httpd config file for test"
+
+cp -f httpd.conf /etc/httpd/conf/httpd.conf 
+cp VirtualHost.conf /etc/httpd/conf.d
+mv networklab/ /var/www/html/
+
+chown ec2-user:ec2-user -R /var/www/html/networklab/
+
+
+echo "################"
+echo "## GOOD LUCK ###"
+echo "################"
