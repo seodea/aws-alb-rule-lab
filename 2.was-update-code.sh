ffc6cfab9f80a543b0a455a7a52e72d99fee2e54
MZC01-SDH <mzc01-sdh@MZC01-SDH.local>
2024-04-08 21:26:51 +0900

    modify file

--- 2.was-update-code.sh
+++ 2.was-update-code.sh
@@ -3,3 +3,6 @@
 cp -f was/httpd.conf /etc/httpd/conf/httpd.conf
 cp was/VirtualHost.conf /etc/httpd/conf.d
 mv was/networklab/ /var/www/html/
+
+chown ec2-user:ec2-user -R /var/www/html/networklab/
+chown apache:apache -R /var/www/html/networklab/was-guestbook/guestbook.txt
